a661baa2d66f3070e94c2d0cb5056a75c9442833
shinyu0430 <0430shinyu@gmail.com>
2022-01-22 22:12:51 +0800

    fix:移除 response.data.success

--- login.js
+++ login.js
@@ -13,14 +13,9 @@ const app = {
         login() {
           const api = 'https://vue3-course-api.hexschool.io/v2/admin/signin';
           axios.post(api, this.user).then((response) => {
-            if(response.data.success) {
               const { token, expired } = response.data;
               document.cookie = `userToken=${token};expires=${new Date(expired)}; path=/`;
               window.location = 'product.html';
-              alert('success');
-            } else {
-              alert(response.data.message);
-            }
           }).catch((error) => {
             alert(error.data.message);
           });
